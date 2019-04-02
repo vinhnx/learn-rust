6092477aeec093a8dce535ff689ebb1c6ef28666
Vinh Nguyen <vinh.nguyenxuan@2359media.com>
2019-04-02 16:11:05 +0700

    Reference stackoverflow page on generic

--- hello_world/pretty_print.rs
+++ hello_world/pretty_print.rs
@@ -1,6 +1,6 @@
 #[derive(Debug)]
 struct Person<'a> {
-  name: &'a str,
+  name: &'a str, // https://stackoverflow.com/a/47641166/1477298
   age: u8
 }
 
@@ -10,3 +10,20 @@ fn main() {
   let person = Person { name, age };
   println!("{:#?}", person);
 }
+
+// #[derive(Debug)]
+// struct AnotherPerson {
+//   name: String,
+//   age: u8
+// }
+
+// fn main() {
+//   // https://stackoverflow.com/a/47641166/1477298
+//   // let name = "Vinh"; // error: expected type `std::string::String`
+//                         // found type `&str`
+//   let name = String::from("Vinh");
+//   let age = 29;
+//   let person = AnotherPerson { name, age };
+
+//   println!("{:#?}", person);
+// }
