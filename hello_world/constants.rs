9eaebd1b635368f0e6edcb434d075e8af01445f1
Vinh Nguyen <vinh.nguyenxuan@2359media.com>
2019-04-05 14:18:41 +0700

    Add constants example

---
+++ hello_world/constants.rs
@@ -0,0 +1,13 @@
+static LANGUAGE: &str = "Rust";
+const THRESHOLD: i32 = 10;
+
+fn is_big(n: i32) -> bool {
+  n > THRESHOLD
+}
+
+fn main() {
+  let n = 16;
+  println!("{}", LANGUAGE);
+  println!("{}", THRESHOLD);
+  println!("{} is {}", n, if is_big(n) { "big"} else { "small"} );
+}
